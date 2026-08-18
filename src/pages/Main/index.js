617edae7966fa7bab3bb51d5543e21a760a3f151
piercearchives_ <rodrigofreezy593@gmail.com>
2026-08-18 18:34:14 -0300

    fetch render posts from API

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,41 @@
 import api from '../../services/api';
 import Post from '../../components/Post';
+import { useState } from 'react';
 
 const Main = () => {
-  return (
-    <>
+
+const [post, setPost] = useState([]);
+
+async function handleAxios(){
+  try {
+    const response = await api.get('/posts');
+    
+    setPost(response.data)
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+return (
+  <>
+  <section className='container'>
     <h1>main.js</h1>
-    </>
-  )
+  <button onClick={handleAxios}>Fazer requisição</button>
+  <div className='mt-5 container-posts'>
+    {
+      post.map(post => {
+        return(
+          
+          <Post subtitle={post.category} title={post.title}>
+            {post.resume}
+          </Post>
+        )
+      })
+    }
+  </div>
+  </section>
+  </>
+)
 }
 
 export default Main;
