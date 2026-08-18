1f908b38b7d82e0b4eb62ad407cb1f6bf6653105
piercearchives_ <rodrigofreezy593@gmail.com>
2026-08-18 16:30:31 -0300

    add useState cart state management

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import Header from './components/Header';
 import Article from './components/Article';
+import {useState} from 'react';
+
 function App() {
+
+  
+  let  [carrinho, setCarrinho]= useState(0);
+
+
+
+
+  function AddItem(){
+    setCarrinho(carrinho + 1);
+    console.log(carrinho);
+  }
+
+  function RemoveItem(){
+    if(carrinho > 0){
+      setCarrinho(carrinho - 1)
+    }
+    console.log(carrinho);
+  }
+
   return (
  <>
       
      <Header />
       <Article />
+      <h1>carrinho de compras: {carrinho}</h1>
+       <div className='flex'>
+         <button className='add-button ml-2' onClick={AddItem}>+</button>
+            <button className='remove-button ml-2' onClick={RemoveItem}>-</button>
+       </div>
  </>
-   
   );
 }
 
