3493e19f98911277ae03b53107568bd52d392879
piercearchives_ <rodrigofreezy593@gmail.com>
2026-08-18 09:29:50 -0300

    add react props and children

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -4,11 +4,17 @@ function Article(){
   return(
  <>
  <section className='container'>
-  <h3>Articles</h3>
+  <h3 className='mx-2'>Articles</h3>
   <div className='mt-3 container-posts'>
-    <Post />
-    <Post />
-    <Post />
+    <Post subtitle="tecnologia" title="o guia definitivo sobre o blog">
+Descubra como navegar perfeitamente pelo nosso site e aproveitar ao máximo todo o conteúdo exclusivo disponível.
+    </Post>
+    <Post subtitle="fotografia" title="quais as melhores câmeras em 2021?">
+Confira nossa seleção especial dos modelos de câmeras mais seletivos e eficientes que se destacaram no mercado.
+    </Post>
+    <Post subtitle="cinema" title="os 10 filmes com maior bilheteria">
+Relembre as maiores produções do cinema mundial que conquistaram o público e bateram recordes históricas de arrecadação.
+    </Post>
   </div>
  </section>
  </>
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,16 +1,16 @@
-const Post = () => {
+const Post = ({title, subtitle, children}) => {
   return (
     <>
     <section className="container">
-      <div className="post">
+      <div className="post mx-2">
         <div className="img-post"></div>
-        <h5>Tecnologia</h5>
-        <h3>o guia definitivo sobre o blog</h3>
-        <p>lorem
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse modi alias rem velit rerum itaque qui consequatur. Illo, minus eveniet earum molestias totam molestiae? Rerum officia eveniet amet necessitatibus iste
+        <h5>{subtitle}</h5>
+        <h3>{title}</h3>
+        <p className="mt-1">
+          {children}
         </p>
 
-        <div className="flex pt-3">
+        <div className="flex pt-2">
           <div className="img-profile"></div>
           <div className="desc-profile ml-2"><h6 className="color-blue">Leonardo Swiley</h6>
           <p>Aug 2, 2020 - 8 min read</p></div>
