d8b0dde40e4e10d5de17e87cf3c67071abcf85c0
piercearchives_ <rodrigofreezy593@gmail.com>
2026-08-18 16:57:47 -0300

    setup axios api service and main page

---
+++ src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import api from '../../services/api';
+import Post from '../../components/Post';
+
+const Main = () => {
+  return (
+    <>
+    <h1>main.js</h1>
+    </>
+  )
+}
+
+export default Main;
