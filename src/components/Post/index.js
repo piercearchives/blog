c9619336dcbfa8b71e2e74e414d87cc4cd670dec
piercearchives_ <rodrigofreezy593@gmail.com>
2026-08-18 23:15:49 -0300

    using effect to load posts automaticallly on mount

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,19 +1,19 @@
-const Post = ({title, subtitle, children}) => {
+const Post = (props) => {
   return (
     <>
     <section className="container">
       <div className="post mx-2">
         <div className="img-post"></div>
-        <h5>{subtitle}</h5>
-        <h3>{title}</h3>
+        <h5>{props.subtitle}</h5>
+        <h3>{props.title}</h3>
         <p className="mt-1">
-          {children}
+          {props.children}
         </p>
 
         <div className="flex pt-2">
           <div className="img-profile"></div>
-          <div className="desc-profile ml-2"><h6 className="color-blue">Leonardo Swiley</h6>
-          <p>Aug 2, 2020 - 8 min read</p></div>
+          <div className="desc-profile ml-2"><h6 className="color-blue">{props.author}</h6>
+          <p>{props.date}</p></div>
           
         </div>
       </div>
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,32 +1,37 @@
 import api from '../../services/api';
 import Post from '../../components/Post';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Main = () => {
 
 const [post, setPost] = useState([]);
 
-async function handleAxios(){
-  try {
-    const response = await api.get('/posts');
-    
-    setPost(response.data)
-  } catch (error) {
-    console.error(error);
-  }
-}
+useEffect(() => {
+  api.get('/posts')
+    .then((response) => {
+      setPost(response.data)
+      console.log(response)
+    })
+}, [])
+
+
 
 return (
   <>
   <section className='container'>
-    <h1>main.js</h1>
-  <button onClick={handleAxios}>Fazer requisição</button>
+   <h2>Articles</h2>
   <div className='mt-5 container-posts'>
     {
       post.map(post => {
         return(
           
-          <Post subtitle={post.category} title={post.title}>
+          <Post
+          key={post.id}
+          subtitle={post.category}
+          title={post.title}
+          author={post.author}
+          date={post.date}
+          >
             {post.resume}
           </Post>
         )
