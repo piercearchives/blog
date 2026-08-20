b5f79cf4ae76f4ea47c64194a304bda608743925
piercearchives_ <rodrigofreezy593@gmail.com>
2026-08-20 06:59:18 -0300

    Adds useState and useEffect with persistence in localStorage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
 import Header from './components/Header';
 import Routes from './routes'
+import { useEffect, useState } from 'react';
 
 function App() {
+const [tarefas, setTarefas] = useState(() => {
+  const tarefasSalvas = localStorage.getItem('Array de tarefas');
 
+  return tarefasSalvas ? JSON.parse(tarefasSalvas) : [];
+});
+
+const [campo, setCampo] = useState('')
+
+function AdicionarItem(){
+  setTarefas([...tarefas, campo]);
+  setCampo('');
+}
+
+function handlecampo(e){
+setCampo(e.target.value);
+
+}
+// funciona como um componentDidMount
+// useEffect(() => {
+// const tarefasStorage = localStorage.getItem('Array de tarefas');
+
+// if(tarefasStorage){
+//   setTarefas(JSON.parse(tarefasStorage));
+// }
+// }, [])
+
+// funciona como um componentDidUpdate
+useEffect(() => {
+  localStorage.setItem('Array de tarefas', JSON.stringify(tarefas))
+}, [tarefas])
   return (
  <>
       
      <Header />
     <Routes />
+<div>
+  <h1>Olá, vamos falar sobre useState</h1> <br/>
+
+  <ul>
+    {
+      tarefas.map((item, index) => (
+        <li key={`${item}-${index}`}>{item}</li>
+      ))}
+  </ul>
+
+  <input type='text' name='nome' value={campo} onChange={(e) => setCampo(e.target.value)}/>
+
+    <button type='button' onClick={AdicionarItem}>adicionar</button>
+
+</div>
+
  </>
   );
 }
