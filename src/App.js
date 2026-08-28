9ad1fe475fd95f31661ce2b029d3354433af3f36
piercearchives_ <rodrigofreezy593@gmail.com>
2026-08-28 15:13:27 -0300

    feat: adicionando login, perfil e formulário de posts

--- src/App.js
+++ src/App.js
@@ -346,6 +346,117 @@ function App() {
 </section>
 
 
+{/* login */}
+
+<section className='container'>
+<div className='row'>
+  <div className='grid-4  disappear' ></div>
+  <div className='grid-4 '>
+    <div className='flex-center'>
+      <img src='/svg/blog-logo2.svg' alt='logoLogin' className='icon-l'/>
+    </div>
+    <h5 className='text-center'>Olá, faça o login para continuar.</h5>
+    
+    <form>
+      <input type='text' name='user' placeholder='Digite o seu usuário' className='mt-3'/> 
+      <input type='password' name='password' placeholder='Digite sua senha' className='mt-2'/> 
+      <button className='btn w-100 mt-4'>Entrar</button>
+    </form>
+  </div>
+  <div className='grid-4 disappear'></div>
+</div>
+</section>
+
+
+{/* profile */}
+<section className='container'>
+  <div className='row'>
+    <div className='grid-6'>
+      <div className='flex-start-row'>
+        <div className='profile-big'>
+          <img src='/img/profile/hp.jpg' alt='perfil do pierce' className='profile-img'></img>
+        </div>
+        <div className='ml-3'>
+          <h3>Rodrigo Pierce</h3>
+          <h6>@piercearchives_</h6>
+        </div>
+        
+      </div>
+      <p className='mt-3'>Lorem ipsum dolor sit amet consectetur. 
+          Tortor dolor in eleifend.</p>
+    </div>
+    <div className='grid-6'>
+      <a href='#meusDados' alt='dados' className='btn'>Meus Dados</a>
+      <a href='#adicionarPost' alt='dados' className='btn ml-3'>Adicionar Post</a>
+    </div>
+  </div>
+</section>
+
+
+{/* adicionar post */}
+<section className='container'>
+<h3>Adicionar um novo post</h3>
+<p className='mt-1'>
+  Preenchar os camps abaixo para adicionar um novo post ao blog.
+</p>
+
+<form>
+
+  <div className='row'>
+  <div className='grid-3 p-0'>
+    <label for='date'><h6>Data</h6></label>
+    <input type='date' id='date' name='date' className='mt-1'/>
+  </div>
+
+  <div className='grid-3 p-0'>
+     <label for='category'><h6>Categoria</h6></label>
+    <select id='category' name='category' className='mt-1'>
+      <option value='tecnologia'>tecnologia</option>
+      <option value='games'>games</option>
+      <option value='fotografia'>fotografia</option>
+      <option value='cinema'>cinema</option>
+    </select>
+  </div>
+
+  <div className='grid-6 p-0'>
+    <label for='title'><h6>Título</h6></label>
+    <input type='text' id='title' name='title' className='mt-1'/>
+  </div>
+</div>
+
+
+
+
+<div className='row'>
+  <div className='grid-9  p-0'>
+     <label for='resume'><h6>Resumo</h6></label>
+    <input type='text' id='resume' name='resume' className='mt-1'/>
+  </div>
+  <div className='grid-3  p-0'>
+    <label for='duration'><h6>Duração</h6></label>
+    <select id='duration' name='duration' className='mt-1'>
+      <option value='tecnologia'>5min</option>
+      <option value='games'>7min</option>
+      <option value='fotografia'>15min</option>
+      <option value='cinema'>9min</option>
+    </select>
+  </div>
+</div>
+
+
+<div className='row'>
+  <div className='grid-12'>
+     <label for='description'><h6>Descrição</h6></label>
+    <textarea name='description' id='description' className='w-100' rows='10'></textarea>
+  </div>
+</div>
+
+<div className='row flex-end-row'>
+<button className='btn mr-2'> Adicionar</button>
+</div>
+</form>
+</section>
+
     </>
   );
 }
